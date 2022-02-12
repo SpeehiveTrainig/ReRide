6b27b550b9b93c582cafa591414f6835c8a44095
vishnukumar.ps@outlook.com <vishnukumar.ps@outlook.com>
2022-02-12 12:44:47 +0530

    sync

---
+++ ReRideWebsite/ViewModel/CarViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace ReRideWebsite.ViewModel
+{
+    public class CarViewModel
+    {
+
+     
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; } = "Test";
+        public DateTime RegistrationDate { get; set; }
+
+
+        public DateTime CreatedTime { get; set; } = DateTime.Now;
+        public DateTime ModifiedTime { get; set; } = DateTime.Now;
+
+
+        public string NikName { get; set; }
+
+
+    }
+}
