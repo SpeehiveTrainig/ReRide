be132092e00190c4680a1ab6cdd2ab70324cd5a5
Ashiq Nazar <khanashiq18@gmail.com>
2022-03-08 09:10:16 +0530

    AddressLine3

--- ReRideWebAPI/Models/Employee.cs
+++ ReRideWebAPI/Models/Employee.cs
@@ -5,6 +5,8 @@
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string address{ get; set; }
+
+        public string  AddressLine3 { get; set; }
         public int Age { get; set; }
 
 
