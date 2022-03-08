ecee19b853bbf107b283f38b3737e380b2ff13a1
RINSHAD PR <Rinshad.pr@outlook.com>
2022-03-07 20:57:17 -0800

    Employee model added

---
+++ ReRideWebAPI/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace ReRideWebAPI.Models
+{
+    public class Employee
+    {
+        public string Id { get; set; }
+        public int MyProperty { get; set; }
+        public string Id2 { get; set; }
+        public int MyProperty1 { get; set; }
+
+
+
+
+
+    }
+}
